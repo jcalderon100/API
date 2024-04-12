9afb4cc458a877610c9a3fe3c1a5273d26807e9a
Julio Calderón <julio.calderon.100@gmail.com>
2024-04-11 23:01:15 -0700

    lista de criptomonedas

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,39 @@
+import { useEffect, useState } from "react"
+import axios from "axios"
+import "./App.css"
 
 function App() {
+
+  const API_URL = import.meta.env.VITE_API_URL
+
+  const [criptos, setCriptos] = useState ()
   
+  useEffect (() => {
+    axios.get(`${API_URL}assets`)
+    .then((data) => {
+      console.log(data)
+      setCriptos(data.data.data)
+    })
+    .catch(()=>{
+      console.log("la peticion fallo")
+    })
+  }, [])
+
+  if(!criptos) return <span>Cargando...</span>
+
 
   return (
     <>
-    <h1>hola mundo</h1>
+    <h1>Lista de Criptomonedas</h1>
+    <ol>
+      {criptos.map(({name, priceUsd}) => (
+        <li>Nombre: {name} Precio: {priceUsd}</li>
+      ))}
+    </ol>
     </>
   )
 }
 
 export default App
+
+
